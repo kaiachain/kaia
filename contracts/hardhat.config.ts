e4acff70bb91d6ab8349b1ed8ad6e11755e7b932
Chihyun Song <ian0371@gmail.com>
2024-03-25 17:11:29 +0900

    Restructure directories

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,20 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.4.24",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+      {
+        version: "0.8.18",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      },
+    ],
+  },
+};
+
+export default config;
