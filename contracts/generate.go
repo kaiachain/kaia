ccd76f865dd72c24692c61d4d59ecf60890d1d2c
ollie.j <ollie.j@klaytn.foundation>
2024-04-08 17:35:14 +0900

    contracts: Resolve abigen and hardhat conflict
    abigen requires @openzeppelin symlink into node_modules
    hardhat is irritated by the symlink. The abigenw script
    temporarily creates the symlink and deletes afterwards.

--- contracts/generate.go
+++ contracts/generate.go
@@ -17,35 +17,37 @@
 package contracts
 
 /*
-You can use solc-select or svm-rs to quickly switch solc versions.
-Example:
-	solc-select use 0.4.24
+Recommended to install solc-select or svm-rs to switch solc versions within abigenw.
+
+	solc-select install 0.4.24 0.5.6 0.8.19
+	go generate
+
+Othewise, you can manually switch solc versions and run go generate for each solc version.
+
 	go generate --run 0.4.24
-	solc-select use 0.5.6
 	go generate --run 0.5.6
-	solc-select use 0.8.19
 	go generate --run 0.8.19
 */
 
 // These files were compiled with solidity 0.4.24.
 
-//go:generate abigen --pkg misc --sol ./contracts/system_contracts/misc/credit.sol --out ./contracts/system_contracts/misc/credit.go # 0.4.24
-//go:generate abigen --pkg consensus --sol ./contracts/system_contracts/consensus/consensus.sol --out ./contracts/system_contracts/consensus/consensus.go # 0.4.24
-//go:generate abigen --pkg reward --sol ./contracts/testing/reward/all.sol --out ./contracts/testing/reward/all.go # 0.4.24
+//go:generate ./abigenw --pkg misc --sol ./contracts/system_contracts/misc/credit.sol --out ./contracts/system_contracts/misc/credit.go --ver 0.4.24
+//go:generate ./abigenw --pkg consensus --sol ./contracts/system_contracts/consensus/consensus.sol --out ./contracts/system_contracts/consensus/consensus.go --ver 0.4.24
+//go:generate ./abigenw --pkg reward --sol ./contracts/testing/reward/all.sol --out ./contracts/testing/reward/all.go --ver 0.4.24
 
 // These files were compiled with solidity 0.5.6.
 
-//go:generate abigen --pkg bridge --sol ./contracts/service_chain/bridge/Bridge.sol --out ./contracts/service_chain/bridge/bridge.go # 0.5.6
-//go:generate abigen --pkg sc_erc20 --sol ./contracts/testing/sc_erc20/sc_token.sol --out ./contracts/testing/sc_erc20/sc_token.go # 0.5.6
-//go:generate abigen --pkg sc_erc721 --sol ./contracts/testing/sc_erc721/sc_nft.sol --out ./contracts/testing/sc_erc721/sc_nft.go # 0.5.6
-//go:generate abigen --pkg sc_erc721_no_uri --sol ./contracts/testing/sc_erc721_no_uri/sc_nft_no_uri.sol --out ./contracts/testing/sc_erc721_no_uri/sc_nft_no_uri.go # 0.5.6
-//go:generate abigen --pkg extbridge --sol ./contracts/testing/extbridge/ext_bridge.sol --out ./contracts/testing/extbridge/ext_bridge.go # 0.5.6
+//go:generate ./abigenw --pkg bridge --sol ./contracts/service_chain/bridge/Bridge.sol --out ./contracts/service_chain/bridge/bridge.go --ver 0.5.6
+//go:generate ./abigenw --pkg sc_erc20 --sol ./contracts/testing/sc_erc20/sc_token.sol --out ./contracts/testing/sc_erc20/sc_token.go --ver 0.5.6
+//go:generate ./abigenw --pkg sc_erc721 --sol ./contracts/testing/sc_erc721/sc_nft.sol --out ./contracts/testing/sc_erc721/sc_nft.go --ver 0.5.6
+//go:generate ./abigenw --pkg sc_erc721_no_uri --sol ./contracts/testing/sc_erc721_no_uri/sc_nft_no_uri.sol --out ./contracts/testing/sc_erc721_no_uri/sc_nft_no_uri.go --ver 0.5.6
+//go:generate ./abigenw --pkg extbridge --sol ./contracts/testing/extbridge/ext_bridge.sol --out ./contracts/testing/extbridge/ext_bridge.go --ver 0.5.6
 
 // These files were compiled with solidity 0.8.19.
 
-//go:generate abigen --pkg gov --sol ./contracts/system_contracts/gov/GovParam.sol --out ./contracts/system_contracts/gov/GovParam.go # 0.8.19
-//go:generate abigen --pkg kip103 --sol ./contracts/system_contracts/kip103/TreasuryRebalance.sol --out ./contracts/system_contracts/kip103/TreasuryRebalance.go # 0.8.19
-//go:generate abigen --pkg kip113 --sol ./contracts/system_contracts/kip113/SimpleBlsRegistry.sol --out ./contracts/system_contracts/kip113/SimpleBlsRegistry.go # 0.8.19
-//go:generate abigen --pkg kip149 --sol ./contracts/system_contracts/kip149/Registry.sol --out ./contracts/system_contracts/kip149/Registry.go # 0.8.19
-//go:generate abigen --pkg proxy --sol ./contracts/system_contracts/proxy/proxy.sol --out ./contracts/system_contracts/proxy/proxy.go # 0.8.19
-//go:generate abigen --pkg system_contracts --sol ./contracts/testing/system_contracts/all.sol --out ./contracts/testing/system_contracts/all.go # 0.8.19
+//go:generate ./abigenw --pkg gov --sol ./contracts/system_contracts/gov/GovParam.sol --out ./contracts/system_contracts/gov/GovParam.go --ver 0.8.19
+//go:generate ./abigenw --pkg kip103 --sol ./contracts/system_contracts/kip103/TreasuryRebalance.sol --out ./contracts/system_contracts/kip103/TreasuryRebalance.go --ver 0.8.19
+//go:generate ./abigenw --pkg kip113 --sol ./contracts/system_contracts/kip113/SimpleBlsRegistry.sol --out ./contracts/system_contracts/kip113/SimpleBlsRegistry.go --ver 0.8.19
+//go:generate ./abigenw --pkg kip149 --sol ./contracts/system_contracts/kip149/Registry.sol --out ./contracts/system_contracts/kip149/Registry.go --ver 0.8.19
+//go:generate ./abigenw --pkg proxy --sol ./contracts/system_contracts/proxy/proxy.sol --out ./contracts/system_contracts/proxy/proxy.go --ver 0.8.19
+//go:generate ./abigenw --pkg system_contracts --sol ./contracts/testing/system_contracts/all.sol --out ./contracts/testing/system_contracts/all.go --ver 0.8.19
