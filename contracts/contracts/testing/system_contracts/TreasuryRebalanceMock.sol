8e558ac9ee7d3b1cf816fc65c523fc65c34381c9
yumiel.ko <yumiel.ko@klaytn.foundation>
2024-04-09 11:07:35 +0800

    fix import path at TreasuryRebalanceMock.sol

--- contracts/contracts/testing/system_contracts/TreasuryRebalanceMock.sol
+++ contracts/contracts/testing/system_contracts/TreasuryRebalanceMock.sol
@@ -2,8 +2,8 @@
 
 pragma solidity ^0.8.0;
 
-import "./TreasuryRebalance.sol";
-import "./TreasuryRebalanceV2.sol";
+import "../../system_contracts/rebalance/TreasuryRebalance.sol";
+//import "./TreasuryRebalanceV2.sol";
 
 contract TreasuryRebalanceMock is TreasuryRebalance {
     constructor(uint256 _rebalanceBlockNumber) TreasuryRebalance(_rebalanceBlockNumber) {}
@@ -31,28 +31,29 @@ contract TreasuryRebalanceMock is TreasuryRebalance {
     }
 }
 
-contract TreasuryRebalanceMockV2 is TreasuryRebalanceV2 {
-    constructor(uint256 _rebalanceBlockNumber) TreasuryRebalanceV2(_rebalanceBlockNumber) {}
+// TODO: will be enabled with kip160
+// contract TreasuryRebalanceMockV2 is TreasuryRebalanceV2 {
+//     constructor(uint256 _rebalanceBlockNumber) TreasuryRebalanceV2(_rebalanceBlockNumber) {}
 
-    function testSetAll(
-        address[] calldata _zeroeds,
-        address[] calldata _allocateds,
-        uint256[] calldata _amounts,
-        uint256 _rebalanceBlockNumber,
-        Status _status
-    ) external {
-        delete zeroeds;
-        delete allocateds;
+//     function testSetAll(
+//         address[] calldata _zeroeds,
+//         address[] calldata _allocateds,
+//         uint256[] calldata _amounts,
+//         uint256 _rebalanceBlockNumber,
+//         Status _status
+//     ) external {
+//         delete zeroeds;
+//         delete allocateds;
 
-        address[] memory empty = new address[](1);
-        for (uint256 i = 0; i < _zeroeds.length; i++) {
-            zeroeds.push(Zeroed(_zeroeds[i], empty));
-        }
-        for (uint256 i = 0; i < _allocateds.length; i++) {
-            allocateds.push(Allocated(_allocateds[i], _amounts[i]));
-        }
+//         address[] memory empty = new address[](1);
+//         for (uint256 i = 0; i < _zeroeds.length; i++) {
+//             zeroeds.push(Zeroed(_zeroeds[i], empty));
+//         }
+//         for (uint256 i = 0; i < _allocateds.length; i++) {
+//             allocateds.push(Allocated(_allocateds[i], _amounts[i]));
+//         }
 
-        rebalanceBlockNumber = _rebalanceBlockNumber;
-        status = _status;
-    }
-}
+//         rebalanceBlockNumber = _rebalanceBlockNumber;
+//         status = _status;
+//     }
+// }
