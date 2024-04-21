d2f31beac77da21378e3bf8cf2e775683f118609
ollie.j <ollie.j@klaytn.foundation>
2024-04-21 19:23:38 +0900

    contracts: Remove comparison against boolean literal

--- contracts/contracts/service_chain/bridge/BridgeTransferERC721.sol
+++ contracts/contracts/service_chain/bridge/BridgeTransferERC721.sol
@@ -84,7 +84,7 @@ contract BridgeTransferERC721 is BridgeTokens, IERC721BridgeReceiver, BridgeTran
     {
         require(isRunning, "stopped bridge");
         (bool success, bytes memory uri) = _tokenAddress.call(abi.encodePacked(ERC721Metadata(_tokenAddress).tokenURI.selector, abi.encode(_tokenId)));
-        if (success == false) {
+        if (!success) {
             uri = "";
         }
         if (modeMintBurn) {
