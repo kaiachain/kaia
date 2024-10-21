789ead74398b32d8572d550538ee2ea5f2039ac3
ollie.j <ollie.j@klaytn.foundation>
2024-10-21 23:45:38 +0900

    kaiax/gov: Remove reward APIs from kaiax/gov

--- kaiax/gov/impl/api.go
+++ kaiax/gov/impl/api.go
@@ -1,19 +1,13 @@
 package impl
 
 import (
-	"errors"
-	"fmt"
 	"math/big"
-	"runtime"
-	"sync"
-	"time"
 
 	"github.com/kaiachain/kaia/common"
 	"github.com/kaiachain/kaia/kaiax/gov"
 	"github.com/kaiachain/kaia/kaiax/gov/headergov"
 	"github.com/kaiachain/kaia/networks/rpc"
 	"github.com/kaiachain/kaia/params"
-	"github.com/kaiachain/kaia/reward"
 )
 
 func (g *GovModule) APIs() []rpc.API {
@@ -63,131 +57,12 @@ type StatusAPI struct {
 	MyVotes      []headergov.VoteData              `json:"myVotes"`
 }
 
-type AccumulatedRewardsResponse struct {
-	FirstBlockTime string   `json:"firstBlockTime"`
-	LastBlockTime  string   `json:"lastBlockTime"`
-	FirstBlock     *big.Int `json:"firstBlock"`
-	LastBlock      *big.Int `json:"lastBlock"`
-
-	// TotalMinted + TotalTxFee - TotalBurntTxFee = TotalProposerRewards + TotalStakingRewards + TotalKIFRewards + TotalKEFRewards
-	TotalMinted          *big.Int                    `json:"totalMinted"`
-	TotalTxFee           *big.Int                    `json:"totalTxFee"`
-	TotalBurntTxFee      *big.Int                    `json:"totalBurntTxFee"`
-	TotalProposerRewards *big.Int                    `json:"totalProposerRewards"`
-	TotalStakingRewards  *big.Int                    `json:"totalStakingRewards"`
-	TotalKIFRewards      *big.Int                    `json:"totalKIFRewards"`
-	TotalKEFRewards      *big.Int                    `json:"totalKEFRewards"`
-	Rewards              map[common.Address]*big.Int `json:"rewards"`
-}
-
 func NewGovAPI(g *GovModule) *GovAPI {
 	return &GovAPI{g}
 }
 
 func (api *GovAPI) GetParams(num *rpc.BlockNumber) (gov.PartialParamSet, error) {
 	return getParams(api.g, num)
-}
-
-func (api *GovAPI) GetRewardsAccumulated(lower rpc.BlockNumber, upper rpc.BlockNumber) (*AccumulatedRewardsResponse, error) {
-	currentBlock := api.g.chain.CurrentBlock().NumberU64()
-	firstBlock := currentBlock
-	if lower >= rpc.EarliestBlockNumber {
-		firstBlock = uint64(lower.Int64())
-	}
-
-	lastBlock := currentBlock
-	if upper >= rpc.EarliestBlockNumber {
-		lastBlock = uint64(upper.Int64())
-	}
-
-	if firstBlock > lastBlock {
-		return nil, errors.New("the last block number should be equal or larger the first block number")
-	}
-
-	if lastBlock > currentBlock {
-		return nil, errors.New("the last block number should be equal or less than the current block number")
-	}
-
-	blockCount := lastBlock - firstBlock + 1
-	if blockCount > 604800 { // 7 days. naive resource protection
-		return nil, errors.New("block range should be equal or less than 604800")
-	}
-	// initialize structures before request a job
-	accumRewards := &AccumulatedRewardsResponse{}
-	blockRewards := reward.NewRewardSpec()
-	mu := sync.Mutex{} // protect blockRewards
-
-	numWorkers := runtime.NumCPU()
-	reqCh := make(chan uint64, numWorkers)
-	errCh := make(chan error, 1)
-	wg := sync.WaitGroup{}
-
-	// introduce the worker pattern to prevent resource exhaustion
-	for i := 0; i < numWorkers; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			// the minimum digit of request is period to avoid current access to an accArray item
-			for num := range reqCh {
-				bn := rpc.BlockNumber(num)
-				kaiaApi := NewKaiaAPI(api.g)
-				blockReward, err := kaiaApi.GetRewards(&bn)
-				if err != nil {
-					errCh <- err
-					return
-				}
-
-				mu.Lock()
-				blockRewards.Add(blockReward)
-				mu.Unlock()
-			}
-		}()
-	}
-
-	// write the information of the first block
-	header := api.g.chain.GetHeaderByNumber(firstBlock)
-	if header == nil {
-		return nil, fmt.Errorf("the block does not exist (block number: %d)", firstBlock)
-	}
-	accumRewards.FirstBlock = header.Number
-	accumRewards.FirstBlockTime = time.Unix(header.Time.Int64(), 0).String()
-
-	// write the information of the last block
-	header = api.g.chain.GetHeaderByNumber(lastBlock)
-	if header == nil {
-		return nil, fmt.Errorf("the block does not exist (block number: %d)", lastBlock)
-	}
-	accumRewards.LastBlock = header.Number
-	accumRewards.LastBlockTime = time.Unix(header.Time.Int64(), 0).String()
-
-	go func() {
-		defer close(reqCh)
-		for num := firstBlock; num <= lastBlock; num++ {
-			reqCh <- num
-		}
-	}()
-
-	// generate a goroutine to return error early
-	go func() {
-		wg.Wait()
-		close(errCh)
-	}()
-
-	if err := <-errCh; err != nil {
-		return nil, err
-	}
-
-	// collect the accumulated rewards information
-	accumRewards.Rewards = blockRewards.Rewards
-	accumRewards.TotalMinted = blockRewards.Minted
-	accumRewards.TotalTxFee = blockRewards.TotalFee
-	accumRewards.TotalBurntTxFee = blockRewards.BurntFee
-	accumRewards.TotalProposerRewards = blockRewards.Proposer
-	accumRewards.TotalStakingRewards = blockRewards.Stakers
-	accumRewards.TotalKIFRewards = blockRewards.KIF
-	accumRewards.TotalKEFRewards = blockRewards.KEF
-
-	return accumRewards, nil
 }
 
 func (api *GovAPI) NodeAddress() (common.Address, error) {
@@ -204,36 +79,6 @@ func (api *KaiaAPI) GetChainConfig(num *rpc.BlockNumber) *params.ChainConfig {
 
 func (api *KaiaAPI) GetParams(num *rpc.BlockNumber) (gov.PartialParamSet, error) {
 	return getParams(api.g, num)
-}
-
-func (api *KaiaAPI) GetRewards(num *rpc.BlockNumber) (*reward.RewardSpec, error) {
-	blockNumber := uint64(0)
-	if num == nil || *num == rpc.LatestBlockNumber || *num == rpc.PendingBlockNumber {
-		blockNumber = api.g.chain.CurrentBlock().NumberU64()
-	} else {
-		blockNumber = uint64(num.Int64())
-	}
-	// Check if the node has state to calculate the snapshot.
-	err := checkStateForStakingInfo(api.g, blockNumber)
-	if err != nil {
-		return nil, err
-	}
-
-	header := api.g.chain.GetHeaderByNumber(blockNumber)
-	block := api.g.chain.GetBlock(header.Hash(), blockNumber)
-	if block == nil {
-		return nil, errors.New("not found block")
-	}
-	txs, receipts := block.Transactions(), api.g.chain.GetReceiptsByBlockHash(header.Hash())
-	if header == nil {
-		return nil, fmt.Errorf("the block does not exist (block number: %d)", blockNumber)
-	}
-
-	rules := api.g.chain.Config().Rules(new(big.Int).SetUint64(blockNumber))
-	ps := api.g.EffectiveParamSet(blockNumber)
-	rewardParamNum := reward.CalcRewardParamBlock(header.Number.Uint64(), ps.Epoch, rules)
-	rewardParamSet := api.g.EffectiveParamSet(rewardParamNum)
-	return reward.GetBlockReward(header, txs, receipts, rules, rewardParamSet.ToGovParamSet())
 }
 
 func getChainConfig(g *GovModule, num *rpc.BlockNumber) *params.ChainConfig {
