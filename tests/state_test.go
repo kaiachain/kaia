48250fee9097d14ba34ab481a3c078a428cbe9b5
Shogo Hyodo <mmoshg8u@gmail.com>
2024-10-30 12:26:07 +0900

    Add state test

--- tests/state_test.go
+++ tests/state_test.go
@@ -29,6 +29,7 @@ import (
 	"testing"
 
 	"github.com/kaiachain/kaia/blockchain/vm"
+	"github.com/kaiachain/kaia/common"
 	"github.com/kaiachain/kaia/params"
 )
 
@@ -58,21 +59,38 @@ func TestState(t *testing.T) {
 	st.skipLoad(`^stRandom2/randomStatetest642.json`)
 
 	st.walk(t, stateTestDir, func(t *testing.T, name string, test *StateTest) {
-		for _, subtest := range test.Subtests() {
-			subtest := subtest
-			key := fmt.Sprintf("%s/%d", subtest.Fork, subtest.Index)
-			name := name + "/" + key
-			t.Run(key, func(t *testing.T) {
-				if subtest.Fork == "Constantinople" {
-					t.Skip("constantinople not supported yet")
-				}
-				withTrace(t, test.gasLimit(subtest), func(vmconfig vm.Config) error {
-					_, err := test.Run(subtest, vmconfig)
-					return st.checkFailure(t, name, err)
-				})
-			})
-		}
+		execStateTest(t, st, test, name)
 	})
+}
+
+// TestExecutionSpecState runs the test fixtures from execution-spec-tests.
+func TestExecutionSpecState(t *testing.T) {
+	if !common.FileExist(executionSpecStateTestDir) {
+		t.Skipf("directory %s does not exist", executionSpecStateTestDir)
+	}
+	st := new(testMatcher)
+	// st.runonly("eip7702")
+
+	st.walk(t, executionSpecStateTestDir, func(t *testing.T, name string, test *StateTest) {
+		execStateTest(t, st, test, name)
+	})
+}
+
+func execStateTest(t *testing.T, st *testMatcher, test *StateTest, name string) {
+	for _, subtest := range test.Subtests() {
+		subtest := subtest
+		key := fmt.Sprintf("%s/%d", subtest.Fork, subtest.Index)
+		name := name + "/" + key
+		t.Run(key, func(t *testing.T) {
+			if subtest.Fork == "Constantinople" {
+				t.Skip("constantinople not supported yet")
+			}
+			withTrace(t, test.gasLimit(subtest), func(vmconfig vm.Config) error {
+				_, err := test.Run(subtest, vmconfig)
+				return st.checkFailure(t, name, err)
+			})
+		})
+	}
 }
 
 // Transactions with gasLimit above this value will not get a VM trace on failure.
