121788a699ad98f1825136d5b533f1ed6bf72980
Mdaiki0730 <garian0730.develop@gmail.com>
2024-10-31 13:07:07 +0900

    Update Copyright of tx_internal_data_ethereum_set_code.go

--- blockchain/types/tx_internal_data_ethereum_set_code.go
+++ blockchain/types/tx_internal_data_ethereum_set_code.go
@@ -1,20 +1,18 @@
-// Modifications Copyright 2024 The Kaia Authors
-// Copyright 2019 The klaytn Authors
-// This file is part of the klaytn library.
+// Copyright 2024 The Kaia Authors
+// This file is part of the Kaia library.
 //
-// The klaytn library is free software: you can redistribute it and/or modify
+// The Kaia library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The klaytn library is distributed in the hope that it will be useful,
+// The Kaia library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with the klaytn library. If not, see <http://www.gnu.org/licenses/>.
-// Modified and improved for the Kaia development.
+// along with the Kaia library. If not, see <http://www.gnu.org/licenses/>.
 
 package types
 
