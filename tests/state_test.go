4d64e25617cac7a2789b377e1e41385d20abad41
Shogo Hyodo <mmoshg8u@gmail.com>
2024-11-13 16:14:16 +0900

    Stop using slices

--- tests/state_test.go
+++ tests/state_test.go
@@ -109,8 +109,10 @@ func execStateTest(t *testing.T, st *testMatcher, test *StateTest, name string, 
 		key := fmt.Sprintf("%s/%d", subtest.Fork, subtest.Index)
 		name := name + "/" + key
 		t.Run(key, func(t *testing.T) {
-			if slices.Contains(skipForks, subtest.Fork) {
-				t.Skipf("%s not supported yet", subtest.Fork)
+			for _, skip := range skipForks {
+				if skip == subtest.Fork {
+					t.Skipf("%s not supported yet", subtest.Fork)
+				}
 			}
 			withTrace(t, test.gasLimit(subtest), func(vmconfig vm.Config) error {
 				_, err := test.Run(subtest, vmconfig)
