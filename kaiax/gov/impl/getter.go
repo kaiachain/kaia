406cfcf91ddf731d5f86fb0bfd15f5abda7e88d0
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2024-12-11 16:28:26 +0900

    [] Refactored

--- kaiax/gov/impl/getter.go
+++ kaiax/gov/impl/getter.go
@@ -1,8 +1,6 @@
 package impl
 
 import (
-	"math/big"
-
 	"github.com/kaiachain/kaia/kaiax/gov"
 )
 
@@ -27,26 +25,4 @@ func (m *GovModule) EffectiveParamSet(blockNum uint64) gov.ParamSet {
 	}
 
 	return *ret
-}
-
-func (m *GovModule) distill(gp gov.ParamSet, blockNum uint64) gov.ParamSet {
-	rule := m.Chain.Config().Rules(new(big.Int).SetUint64(blockNum))
-
-	// To avoid confusion, override some parameters that are deprecated after hardforks.
-	// e.g., stakingupdateinterval is shown as 86400 but actually irrelevant (i.e. updated every block)
-	if rule.IsKore {
-		// Gini option deprecated since Kore, as All committee members have an equal chance
-		// of being elected block proposers.
-		gp.UseGiniCoeff = false
-	}
-	if rule.IsRandao {
-		// Block proposer is randomly elected at every block with Randao,
-		// no more precalculated proposer list.
-		gp.ProposerUpdateInterval = 1
-	}
-	if rule.IsKaia {
-		// Staking information updated every block since Kaia.
-		gp.StakingUpdateInterval = 1
-	}
-	return gp
 }
