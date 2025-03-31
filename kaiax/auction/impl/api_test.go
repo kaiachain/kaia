c4126cc007dcfd3ea6ddf3eb197c9738a6452086
hyeonLewis <lewis.kim@kaia.io>
2025-03-31 13:43:29 +0900

    kaiax/auction: fix test fail

--- kaiax/auction/impl/api_test.go
+++ kaiax/auction/impl/api_test.go
@@ -31,6 +31,7 @@ func prep() *AuctionModule {
 		Chain:       backend.BlockChain(),
 		Backend:     apiBackend,
 		Downloader:  fakeDownloader,
+		NodeKey:     testNodeKey,
 	})
 	mAuction.bidPool.running = 1
 	mAuction.bidPool.auctioneer = common.HexToAddress("0x96Bd8E216c0D894C0486341288Bf486d5686C5b6")
