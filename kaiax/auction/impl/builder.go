297164fa0988b9a3c1d3ad392b55289cef18c9c8
hyeonLewis <lewis.kim@kaia.io>
2025-03-31 13:23:24 +0900

    kaiax/auction: implement bid bundling

--- kaiax/auction/impl/builder.go
+++ kaiax/auction/impl/builder.go
@@ -28,8 +28,44 @@ import (
 var _ builder.TxBundlingModule = (*AuctionModule)(nil)
 
 func (a *AuctionModule) ExtractTxBundles(txs []*types.Transaction, prevBundles []*builder.Bundle) []*builder.Bundle {
-	// TODO: implement me
-	return nil
+	bundles := []*builder.Bundle{}
+	curBlock := a.Chain.CurrentBlock()
+	if curBlock == nil || atomic.LoadUint32(&a.bidPool.running) == 0 {
+		return bundles
+	}
+
+	miningBlock := curBlock.NumberU64() + 1
+	bidTargetMap := a.bidPool.GetTargetTxMap(miningBlock)
+	if bidTargetMap == nil {
+		return bundles
+	}
+
+	for _, tx := range txs {
+		txHash := tx.Hash()
+		bid, ok := bidTargetMap[txHash]
+		if !ok {
+			continue
+		}
+		b := builder.NewBundle(
+			builder.NewTxOrGenList(a.GetBidTxGenerator(tx, bid)),
+			txHash,
+			true,
+		)
+
+		isConflict := false
+		for _, prev := range append(prevBundles, bundles...) {
+			if prev.IsConflict(b) {
+				isConflict = true
+				break
+			}
+		}
+		if isConflict {
+			continue
+		}
+		bundles = append(bundles, b)
+	}
+
+	return bundles
 }
 
 func (a *AuctionModule) FilterTxs(txs map[common.Address]types.Transactions) {
--- kaiax/auction/impl/getter.go
+++ kaiax/auction/impl/getter.go
@@ -16,6 +16,51 @@
 
 package impl
 
-func (a *AuctionModule) Stats() int {
-	return a.bidPool.stats()
+import (
+	"github.com/kaiachain/kaia/blockchain/system"
+	"github.com/kaiachain/kaia/blockchain/types"
+	"github.com/kaiachain/kaia/common"
+	"github.com/kaiachain/kaia/kaiax/auction"
+	"github.com/kaiachain/kaia/kaiax/builder"
+)
+
+const (
+	AuctionTxMaxGasLimit = uint64(12_000_000)
+)
+
+func (a *AuctionModule) GetBidTxGenerator(tx *types.Transaction, bid *auction.Bid) *builder.TxOrGen {
+	gen := func(nonce uint64) (*types.Transaction, error) {
+		var (
+			chainId           = a.InitOpts.ChainConfig.ChainID
+			signer            = types.LatestSignerForChainID(chainId)
+			auctionEntryPoint = a.bidPool.GetAuctionEntryPoint()
+			key               = a.InitOpts.NodeKey
+		)
+
+		data, err := system.EncodeAuctionCallData(bid)
+		if err != nil {
+			return nil, err
+		}
+
+		tx, err := types.NewTransactionWithMap(types.TxTypeEthereumDynamicFee, map[types.TxValueKeyType]interface{}{
+			types.TxValueKeyNonce:      nonce,
+			types.TxValueKeyTo:         &auctionEntryPoint,
+			types.TxValueKeyAmount:     common.Big0,
+			types.TxValueKeyData:       data,
+			types.TxValueKeyGasLimit:   AuctionTxMaxGasLimit,
+			types.TxValueKeyGasFeeCap:  tx.GasFeeCap(),
+			types.TxValueKeyGasTipCap:  tx.GasTipCap(),
+			types.TxValueKeyAccessList: types.AccessList{},
+			types.TxValueKeyChainID:    chainId,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		err = tx.Sign(signer, key)
+
+		return tx, err
+	}
+
+	return builder.NewTxOrGenFromGen(gen, bid.Hash())
 }
