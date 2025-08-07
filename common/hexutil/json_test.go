4120b7a8c3f9075f355e87f79a12e8c38e499f36
Sawyer <18228063+2dvorak@users.noreply.github.com>
2025-08-07 22:19:24 +0900

    Use bit.UintSize instead of custom calculated size

--- common/hexutil/json_test.go
+++ common/hexutil/json_test.go
@@ -28,6 +28,7 @@ import (
 	"encoding/json"
 	"errors"
 	"math/big"
+	"math/bits"
 	"testing"
 
 	"github.com/holiman/uint256"
@@ -390,7 +391,7 @@ func TestUnmarshalUint(t *testing.T) {
 	for _, test := range unmarshalUintTests {
 		var v Uint
 		err := json.Unmarshal([]byte(test.input), &v)
-		if uintBits == 32 && test.wantErr32bit != nil {
+		if bits.UintSize == 32 && test.wantErr32bit != nil {
 			checkError(t, test.input, err, test.wantErr32bit)
 			continue
 		}
