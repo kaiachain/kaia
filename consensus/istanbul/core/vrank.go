720dfd2f7fcdc43e8c77a3c8b20c45592f675621
hyeonLewis <lewis.kim@kaia.io>
2025-10-15 12:01:46 +0900

    core/vrank: Check log frequency after updating metrics

--- consensus/istanbul/core/vrank.go
+++ consensus/istanbul/core/vrank.go
@@ -142,11 +142,6 @@ func (v *Vrank) LateCommits() []time.Duration {
 
 // Log logs accumulated data in a compressed form
 func (v *Vrank) Log() {
-	// Skip logging if VRankLogFrequency is 0 or not in the logging frequency
-	if VRankLogFrequency == 0 || v.view.Sequence.Uint64()%VRankLogFrequency != 0 {
-		return
-	}
-
 	var (
 		lastCommit  = time.Duration(0)
 		lateCommits = v.LateCommits()
@@ -161,6 +156,11 @@ func (v *Vrank) Log() {
 	v.lastCommit = int64(lastCommit)
 
 	v.updateMetrics()
+
+	// Skip logging if VRankLogFrequency is 0 or not in the logging frequency
+	if VRankLogFrequency == 0 || v.view.Sequence.Uint64()%VRankLogFrequency != 0 {
+		return
+	}
 
 	logger.Info("VRank", "seq", v.view.Sequence.Int64(),
 		"round", v.view.Round.Int64(),
