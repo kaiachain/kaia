bcc733c13fc5ef0c268d648f83d42c242a4f9660
Chihyun Song <ian0371@gmail.com>
2026-01-06 16:03:26 +0900

    Remove vrank encoding
    This reverts commit f8494cc8b76e8ee5e91ed7a19b8e01962a0dc783.

--- consensus/istanbul/core/vrank.go
+++ consensus/istanbul/core/vrank.go
@@ -19,7 +19,6 @@
 package core
 
 import (
-	"encoding/hex"
 	"fmt"
 	"math"
 	"math/big"
@@ -27,20 +26,21 @@ import (
 
 	"github.com/kaiachain/kaia/common"
 	"github.com/kaiachain/kaia/consensus/istanbul"
-	"github.com/kaiachain/kaia/kaiax/valset"
 	"github.com/rcrowley/go-metrics"
 )
 
 type Vrank struct {
-	startTime             time.Time
-	view                  istanbul.View
-	committee             []common.Address
-	threshold             time.Duration
+	startTime            time.Time
+	view                 istanbul.View
+	committee            []common.Address
+	threshold            time.Duration
+	commitArrivalTimeMap map[common.Address]time.Duration
+
+	// metrics
 	firstCommit           int64
 	quorumCommit          int64
 	avgCommitWithinQuorum int64
 	lastCommit            int64
-	commitArrivalTimeMap  map[common.Address]time.Duration
 }
 
 var (
@@ -122,39 +122,8 @@ func (v *Vrank) HandleCommitted(blockNum *big.Int) {
 	}
 }
 
-func (v *Vrank) Bitmap() string {
-	serialized := serialize(v.committee, v.commitArrivalTimeMap)
-	assessed := assessBatch(serialized, v.threshold)
-	compressed := compress(assessed)
-	return hex.EncodeToString(compressed)
-}
-
-func (v *Vrank) LateCommits() []time.Duration {
-	serialized := serialize(v.committee, v.commitArrivalTimeMap)
-	lateCommits := make([]time.Duration, 0)
-	for _, t := range serialized {
-		if assess(t, v.threshold) == vrankArrivedLate {
-			lateCommits = append(lateCommits, t)
-		}
-	}
-	return lateCommits
-}
-
 // Log logs accumulated data in a compressed form
 func (v *Vrank) Log() {
-	var (
-		lastCommit  = time.Duration(0)
-		lateCommits = v.LateCommits()
-	)
-
-	// lastCommit = max(lateCommits)
-	for _, t := range lateCommits {
-		if lastCommit < t {
-			lastCommit = t
-		}
-	}
-	v.lastCommit = int64(lastCommit)
-
 	v.updateMetrics()
 
 	// Skip logging if VRankLogFrequency is 0 or not in the logging frequency
@@ -164,8 +133,6 @@ func (v *Vrank) Log() {
 
 	logger.Info("VRank", "seq", v.view.Sequence.Int64(),
 		"round", v.view.Round.Int64(),
-		"bitmap", v.Bitmap(),
-		"late", encodeDurationBatch(lateCommits),
 	)
 }
 
@@ -196,71 +163,6 @@ func (v *Vrank) isTargetCommit(msg *istanbul.Subject, src common.Address) bool {
 		return false
 	}
 	return true
-}
-
-// assess determines if given time is early, late, or not arrived
-func assess(t, threshold time.Duration) uint8 {
-	if t == vrankNotArrivedPlaceholder {
-		return vrankNotArrived
-	}
-
-	if t > threshold {
-		return vrankArrivedLate
-	} else {
-		return vrankArrivedEarly
-	}
-}
-
-func assessBatch(ts []time.Duration, threshold time.Duration) []uint8 {
-	ret := make([]uint8, len(ts))
-	for i, t := range ts {
-		ret[i] = assess(t, threshold)
-	}
-	return ret
-}
-
-// serialize serializes arrivalTime hashmap into array.
-// If committee is sorted, we can simply figure out the validator position in the output array
-// by sorting the output of `kaia.getCommittee()`
-func serialize(committee []common.Address, arrivalTimeMap map[common.Address]time.Duration) []time.Duration {
-	sortedCommittee := valset.NewAddressSet(committee).List()
-
-	serialized := make([]time.Duration, len(sortedCommittee))
-	for i, v := range sortedCommittee {
-		val, ok := arrivalTimeMap[v]
-		if ok {
-			serialized[i] = val
-		} else {
-			serialized[i] = vrankNotArrivedPlaceholder
-		}
-
-	}
-	return serialized
-}
-
-// compress compresses data into 2-bit bitmap
-// e.g., [1, 0, 2] => [0b01_00_10_00]
-func compress(arr []uint8) []byte {
-	zip := func(a, b, c, d uint8) byte {
-		a &= 0b11
-		b &= 0b11
-		c &= 0b11
-		d &= 0b11
-		return byte(a<<6 | b<<4 | c<<2 | d<<0)
-	}
-
-	// pad zero to make len(arr)%4 == 0
-	for len(arr)%4 != 0 {
-		arr = append(arr, 0)
-	}
-
-	ret := make([]byte, len(arr)/4)
-
-	for i := 0; i < len(arr)/4; i++ {
-		chunk := arr[4*i : 4*(i+1)]
-		ret[i] = zip(chunk[0], chunk[1], chunk[2], chunk[3])
-	}
-	return ret
 }
 
 // encodeDuration encodes given duration into string
--- consensus/istanbul/core/vrank_test.go
+++ consensus/istanbul/core/vrank_test.go
@@ -19,15 +19,12 @@
 package core
 
 import (
-	"encoding/hex"
 	"math/big"
 	"testing"
 	"time"
 
-	"github.com/kaiachain/kaia/common"
 	"github.com/kaiachain/kaia/consensus/istanbul"
 	"github.com/kaiachain/kaia/kaiax/valset"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestVrank(t *testing.T) {
@@ -53,83 +50,5 @@ func TestVrank(t *testing.T) {
 		vrank.AddCommit(msg, committee[i])
 		expectedAssessList = append(expectedAssessList, vrankArrivedLate)
 		expectedLateCommits = append(expectedLateCommits, vrank.commitArrivalTimeMap[committee[i]])
-	}
-
-	bitmap, late := vrank.Bitmap(), vrank.LateCommits()
-	assert.Equal(t, hex.EncodeToString(compress(expectedAssessList)), bitmap)
-	assert.Equal(t, expectedLateCommits, late)
-}
-
-func TestVrankAssessBatch(t *testing.T) {
-	arr := []time.Duration{0, 4, 1, vrankNotArrivedPlaceholder, 2}
-	threshold := time.Duration(2)
-	expected := []uint8{
-		vrankArrivedEarly, vrankArrivedLate, vrankArrivedEarly, vrankNotArrived, vrankArrivedEarly,
-	}
-	assert.Equal(t, expected, assessBatch(arr, threshold))
-}
-
-func TestVrankSerialize(t *testing.T) {
-	var (
-		N            = 6
-		committee, _ = genValidators(N)
-		timeMap      = make(map[common.Address]time.Duration)
-		expected     = make([]time.Duration, len(committee))
-	)
-
-	committee = valset.NewAddressSet(committee).List() // sort it
-	for i, val := range committee {
-		t := time.Duration((i * 100) * int(time.Millisecond))
-		timeMap[val] = t
-		expected[i] = t
-	}
-
-	assert.Equal(t, expected, serialize(committee, timeMap))
-}
-
-func TestVrankCompress(t *testing.T) {
-	tcs := []struct {
-		input    []uint8
-		expected []byte
-	}{
-		{
-			input:    []uint8{2},
-			expected: []byte{0b10_000000},
-		},
-		{
-			input:    []uint8{2, 1},
-			expected: []byte{0b10_01_0000},
-		},
-		{
-			input:    []uint8{0, 2, 1},
-			expected: []byte{0b00_10_01_00},
-		},
-		{
-			input:    []uint8{0, 2, 1, 1},
-			expected: []byte{0b00_10_01_01},
-		},
-		{
-			input:    []uint8{1, 2, 1, 2, 1},
-			expected: []byte{0b01_10_01_10, 0b01_000000},
-		},
-		{
-			input:    []uint8{1, 2, 1, 2, 1, 2},
-			expected: []byte{0b01_10_01_10, 0b01_10_0000},
-		},
-		{
-			input:    []uint8{1, 2, 1, 2, 1, 2, 1},
-			expected: []byte{0b01_10_01_10, 0b01_10_01_00},
-		},
-		{
-			input:    []uint8{1, 2, 1, 2, 1, 2, 0, 2},
-			expected: []byte{0b01_10_01_10, 0b01_10_00_10},
-		},
-		{
-			input:    []uint8{1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 2, 2, 1, 1},
-			expected: []byte{0b01011010, 0b01011010, 0b01011010, 0b01011010, 0b01010000},
-		},
-	}
-	for i, tc := range tcs {
-		assert.Equal(t, tc.expected, compress(tc.input), "tc %d failed", i)
 	}
 }
