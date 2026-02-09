98a2ad8d5d6b5abd449ac764aafd279f050e9c22
Chihyun Song <ian0371@gmail.com>
2026-02-09 17:13:53 +0900

    Add TestHandleIstanbulPreprepare

--- kaiax/vrank/impl/handler_test.go
+++ kaiax/vrank/impl/handler_test.go
@@ -65,6 +65,16 @@ func createCN(t *testing.T, valset valset.ValsetModule) *CN {
 	}
 }
 
+func ensurePop(t *testing.T, sub chan *vrank.BroadcastRequest) *vrank.BroadcastRequest {
+	select {
+	case req := <-sub:
+		return req
+	case <-time.After(2 * time.Second):
+		t.Fatal("should broadcast")
+	}
+	return nil
+}
+
 func TestVRankModule(t *testing.T) {
 	var (
 		valset = mock_valset.NewMockValsetModule(gomock.NewController(t))
@@ -89,14 +99,14 @@ func TestVRankModule(t *testing.T) {
 	assert.Equal(t, time.Time{}, val.VRankModule.prepreparedTime)
 	val.VRankModule.HandleIstanbulPreprepare(block, view)
 	assert.NotEqual(t, time.Time{}, val.VRankModule.prepreparedTime)
-	req := <-val.sub
+	req := ensurePop(t, val.sub)
 	assert.Equal(t, []common.Address{cand.Addr}, req.Targets)
 	assert.Equal(t, VRankPreprepareMsg, req.Code)
 	assert.Equal(t, &pppMsg, req.Msg)
 
 	// candidate correctly broadcast VRankCandidate upon receiving VRankPreprepare
 	cand.VRankModule.HandleVRankPreprepare(&pppMsg)
-	req = <-cand.sub
+	req = ensurePop(t, cand.sub)
 	assert.Equal(t, []common.Address{val.Addr}, req.Targets)
 	assert.Equal(t, VRankCandidateMsg, req.Code)
 	assert.Equal(t, &candMsg, req.Msg)
@@ -107,4 +117,58 @@ func TestVRankModule(t *testing.T) {
 	d, ok := val.VRankModule.candResponses.Load(cand.Addr)
 	assert.True(t, ok)
 	assert.NotEqual(t, time.Duration(0), d.(time.Duration))
+}
+
+func TestHandleIstanbulPreprepare(t *testing.T) {
+	var (
+		block1  = types.NewBlockWithHeader(&types.Header{Number: big.NewInt(1)})
+		view1_0 = &istanbul.View{Sequence: big.NewInt(1), Round: common.Big0}
+	)
+
+	t.Run("permissionless fork is disabled", func(t *testing.T) {
+		valset := mock_valset.NewMockValsetModule(gomock.NewController(t))
+		val := createCN(t, valset)
+		val.VRankModule.ChainConfig.PermissionlessCompatibleBlock = nil
+		val.VRankModule.HandleIstanbulPreprepare(block1, view1_0)
+		assert.Equal(t, time.Time{}, val.VRankModule.prepreparedTime)
+		select {
+		case <-val.sub:
+			t.Fatal("under disabled permissionless fork, it should not broadcast")
+		default:
+		}
+	})
+
+	t.Run("non-proposers should not broadcast", func(t *testing.T) {
+		valset := mock_valset.NewMockValsetModule(gomock.NewController(t))
+		var vals []*CN
+		for i := 0; i < 3; i++ {
+			vals = append(vals, createCN(t, valset))
+		}
+		proposer := vals[0]
+		nonProposer := vals[1]
+		candidate := vals[2]
+
+		valset.EXPECT().GetCouncil(uint64(2)).Return([]common.Address{proposer.Addr, nonProposer.Addr}, nil).Times(len(vals))
+		valset.EXPECT().GetProposer(uint64(1), uint64(0)).Return(proposer.Addr, nil).Times(len(vals))
+		valset.EXPECT().GetCandidates(uint64(1)).Return([]common.Address{candidate.Addr}, nil).Times(len(vals))
+
+		proposer.VRankModule.HandleIstanbulPreprepare(block1, view1_0)
+		nonProposer.VRankModule.HandleIstanbulPreprepare(block1, view1_0)
+		candidate.VRankModule.HandleIstanbulPreprepare(block1, view1_0)
+
+		assert.NotEqual(t, time.Time{}, proposer.VRankModule.prepreparedTime)
+		assert.NotEqual(t, time.Time{}, nonProposer.VRankModule.prepreparedTime)
+		assert.Equal(t, time.Time{}, candidate.VRankModule.prepreparedTime) // candidate should not preprepare
+
+		req := ensurePop(t, proposer.sub)
+		assert.Equal(t, []common.Address{candidate.Addr}, req.Targets)
+
+		select {
+		case <-nonProposer.sub:
+			t.Fatal("non-proposer should not broadcast")
+		case <-candidate.sub:
+			t.Fatal("candidate should not broadcast")
+		default:
+		}
+	})
 }
