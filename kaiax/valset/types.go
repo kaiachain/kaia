87fdf2172ab8685951cb51f5f9c31511259f7fec
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2026-02-12 09:22:19 +0900

    [Valset] Defined valset state types

---
+++ kaiax/valset/types.go
@@ -0,0 +1,164 @@
+package valset
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/kaiachain/kaia/common"
+)
+
+type State uint8
+
+const (
+	Unknown State = iota
+	CandInactive
+	CandReady
+	CandTesting
+	ValInactive
+	ValPaused
+	ValExiting
+	ValReady
+	ValActive
+)
+
+const (
+	CandInactiveStr = "CandInactive"
+	CandReadyStr    = "CandReady"
+	CandTestingStr  = "CandTesting"
+	ValInactiveStr  = "ValInactive"
+	ValPausedStr    = "ValPaused"
+	ValExitingStr   = "ValExiting"
+	ValReadyStr     = "ValReady"
+	ValActiveStr    = "ValActive"
+)
+
+func (s State) ToUint8() uint8 {
+	return uint8(s)
+}
+
+func (s State) String() string {
+	switch s {
+	case CandInactive:
+		return CandInactiveStr
+	case CandReady:
+		return CandReadyStr
+	case CandTesting:
+		return CandTestingStr
+	case ValInactive:
+		return ValInactiveStr
+	case ValPaused:
+		return ValPausedStr
+	case ValExiting:
+		return ValExitingStr
+	case ValReady:
+		return ValReadyStr
+	case ValActive:
+		return ValActiveStr
+	default:
+		return fmt.Sprintf("UnknownState(%d)", s)
+	}
+}
+
+func ParseState(s string) (State, error) {
+	switch s {
+	case CandInactiveStr:
+		return CandInactive, nil
+	case CandReadyStr:
+		return CandReady, nil
+	case CandTestingStr:
+		return CandTesting, nil
+	case ValInactiveStr:
+		return ValInactive, nil
+	case ValPausedStr:
+		return ValPaused, nil
+	case ValExitingStr:
+		return ValExiting, nil
+	case ValReadyStr:
+		return ValReady, nil
+	case ValActiveStr:
+		return ValActive, nil
+	default:
+		return 0, fmt.Errorf("invalid state string: %s", s)
+	}
+}
+
+func (s *State) UnmarshalJSON(b []byte) error {
+	var str string
+	if err := json.Unmarshal(b, &str); err != nil {
+		return err
+	}
+	val, err := ParseState(str)
+	if err != nil {
+		return err
+	}
+	*s = val
+	return nil
+}
+
+func (s State) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%q", s.String())), nil
+}
+
+type ValidatorChart struct {
+	State         State     `json:"state"`
+	StakingAmount float64   `json:"stakingAmount"` // in KAIA unit
+	IdleTimeout   time.Time `json:"idleTimeout"`
+	PausedTimeout time.Time `json:"pausedTimeout"`
+}
+
+func (v *ValidatorChart) IsDeepEqual(other *ValidatorChart) bool {
+	if v == other {
+		return true
+	}
+	if v == nil || other == nil {
+		return false
+	}
+	return v.State == other.State &&
+		v.StakingAmount == other.StakingAmount &&
+		v.IdleTimeout.Equal(other.IdleTimeout) &&
+		v.PausedTimeout.Equal(other.PausedTimeout)
+}
+
+type ValidatorChartMap map[common.Address]*ValidatorChart
+
+func (v ValidatorChartMap) String() string {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return fmt.Sprintf("ValidatorChartMap error: %v", err)
+	}
+	return string(b)
+}
+
+func (v ValidatorChartMap) Copy() ValidatorChartMap {
+	if v == nil {
+		return nil
+	}
+
+	cp := make(ValidatorChartMap, len(v))
+	for key, value := range v {
+		if value == nil {
+			cp[key] = nil
+			continue
+		}
+		newValue := *value
+		cp[key] = &newValue
+	}
+	return cp
+}
+
+func (v ValidatorChartMap) Equal(other ValidatorChartMap) bool {
+	if len(v) != len(other) {
+		return false
+	}
+	for addr, val := range v {
+		otherVal, exists := other[addr]
+		if !exists {
+			return false
+		}
+		if !val.IsDeepEqual(otherVal) {
+			return false
+		}
+	}
+	return true
+}
