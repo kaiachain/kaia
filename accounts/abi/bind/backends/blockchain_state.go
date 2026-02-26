b097643550f65641526722666599f4376155b328
Hyunsoo Shin <hyunsooda@kaist.ac.kr>
2026-02-26 16:36:41 +0900

    [Bind] Added `StateBlockchainContractCaller` which implements `ContractCaller`

--- accounts/abi/bind/backends/blockchain_state.go
+++ accounts/abi/bind/backends/blockchain_state.go
@@ -20,37 +20,35 @@ package backends
 
 import (
 	"context"
+	"errors"
 	"math/big"
 
 	"github.com/kaiachain/kaia"
 	"github.com/kaiachain/kaia/accounts/abi/bind"
+	"github.com/kaiachain/kaia/blockchain"
 	"github.com/kaiachain/kaia/blockchain/state"
 	"github.com/kaiachain/kaia/blockchain/types"
+	"github.com/kaiachain/kaia/blockchain/vm"
 	"github.com/kaiachain/kaia/common"
-	"github.com/kaiachain/kaia/node/cn/filters"
+	"github.com/kaiachain/kaia/common/math"
 )
 
-// `StateBlockchainContractBackend` has the same internal behavior with BlockchainContractBackend
-// except for the `ContractCaller` interface functions i.e., `CodeAt()` and `CallContract()`
-type StateBlockchainContractBackend struct {
+// `StateBlockchainContractCaller` implements `ContractCaller` interface
+type StateBlockchainContractCaller struct {
+	bc    BlockChainForCaller
 	state *state.StateDB
-	*BlockchainContractCommonBackend
 }
 
-var (
-	_ bind.ContractCaller     = (*StateBlockchainContractBackend)(nil)
-	_ bind.ContractTransactor = (*StateBlockchainContractBackend)(nil)
-	_ bind.ContractFilterer   = (*StateBlockchainContractBackend)(nil)
-	_ bind.DeployBackend      = (*StateBlockchainContractBackend)(nil)
-	_ bind.ContractBackend    = (*StateBlockchainContractBackend)(nil)
-)
+var _ bind.ContractCaller = (*StateBlockchainContractCaller)(nil)
 
-func NewStateBlockchainContractBackend(bc BlockChainForCaller, tp TxPoolForCaller, es *filters.EventSystem, state *state.StateDB) *StateBlockchainContractBackend {
-	bcCommonBackend := newBlockchainContractCommonBackend(bc, tp, es)
-	return &StateBlockchainContractBackend{state, bcCommonBackend}
+func NewStateBlockchainContractBackend(bc BlockChainForCaller, state *state.StateDB) (*StateBlockchainContractCaller, error) {
+	if state == nil {
+		return nil, errors.New("statedb is nil")
+	}
+	return &StateBlockchainContractCaller{bc, state}, nil
 }
 
-func (b *StateBlockchainContractBackend) getBlock(num *big.Int) (*types.Block, error) {
+func (b *StateBlockchainContractCaller) getBlock(num *big.Int) (*types.Block, error) {
 	var block *types.Block
 	if num == nil {
 		block = b.bc.CurrentBlock()
@@ -69,14 +67,72 @@ func (b *StateBlockchainContractBackend) getBlock(num *big.Int) (*types.Block, e
 
 // bind.ContractCaller defined methods
 
-func (b *StateBlockchainContractBackend) CodeAt(ctx context.Context, account common.Address, blockNumber *big.Int) ([]byte, error) {
-	return b.BlockchainContractCommonBackend.CodeAt(ctx, b.state, account, blockNumber)
+func (b *StateBlockchainContractCaller) CodeAt(ctx context.Context, account common.Address, blockNumber *big.Int) ([]byte, error) {
+	return b.state.GetCode(account), nil
 }
 
-func (b *StateBlockchainContractBackend) CallContract(ctx context.Context, call kaia.CallMsg, blockNumber *big.Int) ([]byte, error) {
+func (b *StateBlockchainContractCaller) CallContract(ctx context.Context, call kaia.CallMsg, blockNumber *big.Int) ([]byte, error) {
 	block, err := b.getBlock(blockNumber)
 	if err != nil {
 		return nil, err
 	}
-	return b.BlockchainContractCommonBackend.CallContract(ctx, b.state, block, call, blockNumber)
+	res, err := b.callContract(call, block, b.state)
+	if err != nil {
+		return nil, err
+	}
+	if len(res.Revert()) > 0 {
+		return nil, blockchain.NewRevertError(res)
+	}
+	return res.Return(), res.Unwrap()
+}
+
+func (b *StateBlockchainContractCaller) callContract(call kaia.CallMsg, block *types.Block, state *state.StateDB) (*blockchain.ExecutionResult, error) {
+	gasPrice := common.Big0
+	if call.GasPrice != nil && (call.GasFeeCap != nil || call.GasTipCap != nil) {
+		return nil, errors.New("both gasPrice and (maxFeePerGas or maxPriorityFeePerGas) specified")
+	}
+	if !b.bc.Config().IsKaiaForkEnabled(block.Number()) { // before KIP-162
+		if call.GasPrice != nil {
+			gasPrice = call.GasPrice
+		}
+	} else { // after KIP-162
+		if call.GasPrice != nil {
+			gasPrice = call.GasPrice
+		} else {
+			if call.GasFeeCap == nil {
+				call.GasFeeCap = big.NewInt(0)
+			}
+			if call.GasTipCap == nil {
+				call.GasTipCap = big.NewInt(0)
+			}
+			gasPrice = math.BigMin(new(big.Int).Add(call.GasTipCap, block.Header().BaseFee), call.GasFeeCap)
+		}
+	}
+
+	if call.Gas == 0 {
+		call.Gas = uint64(3e8) // enough gas for ordinary contract calls
+	}
+
+	var accessList types.AccessList
+	if call.AccessList != nil {
+		accessList = *call.AccessList
+	}
+	intrinsicGas, err := types.IntrinsicGas(call.Data, accessList, nil, call.To == nil, b.bc.Config().Rules(block.Number()))
+	if err != nil {
+		return nil, err
+	}
+
+	msg := types.NewMessage(call.From, call.To, 0, call.Value, call.Gas, gasPrice, nil, nil, nil, call.Data,
+		false, intrinsicGas, accessList, nil, nil, nil, nil)
+
+	txContext := blockchain.NewEVMTxContext(msg, block.Header(), b.bc.Config())
+	blockContext := blockchain.NewEVMBlockContext(block.Header(), b.bc, nil)
+
+	// EVM demands the sender to have enough KAIA balance (gasPrice * gasLimit) in buyGas()
+	// After KIP-71, gasPrice is nonzero baseFee, regardless of the msg.gasPrice (usually 0)
+	// But our sender (usually 0x0) won't have enough balance. Instead we override gasPrice = 0 here
+	txContext.GasPrice = big.NewInt(0)
+	evm := vm.NewEVM(blockContext, txContext, state, b.bc.Config(), &vm.Config{})
+
+	return blockchain.ApplyMessage(evm, msg)
 }
