c26f0cde5e766d02638c58c0d38cbff036d0d292
hyeonLewis <lewis.kim@kaia.io>
2026-04-14 18:46:54 +0900

    consensus/engine: route NewEngine to kaiabft by engine type

--- consensus/engine/engine.go
+++ consensus/engine/engine.go
@@ -7,10 +7,20 @@ import (
 	"github.com/kaiachain/kaia/consensus"
 	"github.com/kaiachain/kaia/consensus/istanbul"
 	istanbulBackend "github.com/kaiachain/kaia/consensus/istanbul/backend"
+	"github.com/kaiachain/kaia/consensus/kaiabft"
+)
+
+// EngineType identifies the consensus engine variant.
+type EngineType string
+
+const (
+	Istanbul EngineType = "istanbul"
+	KaiaBFT  EngineType = "kaiabft"
 )
 
 // Opts bundles the inputs needed to instantiate a consensus engine.
 type Opts struct {
+	EngineType     EngineType
 	IstanbulConfig *istanbul.Config
 	PrivateKey     *ecdsa.PrivateKey
 	NodeType       common.ConnType
@@ -21,6 +31,18 @@ func NewEngine(opts *Opts) consensus.Engine {
 	if opts == nil || opts.IstanbulConfig == nil || opts.PrivateKey == nil {
 		panic("NewEngine receives invalid opts")
 	}
+
+	sealer := istanbul.NewSealerImpl(opts.PrivateKey)
+
+	if opts.EngineType == KaiaBFT {
+		return kaiabft.New(&kaiabft.Opts{
+			Timeout:    opts.IstanbulConfig.Timeout,
+			PrivateKey: opts.PrivateKey,
+			NodeType:   opts.NodeType,
+			Sealer:     sealer,
+		})
+	}
+
 	return istanbulBackend.New(&istanbulBackend.BackendOpts{
 		IstanbulConfig: opts.IstanbulConfig,
 		PrivateKey:     opts.PrivateKey,
