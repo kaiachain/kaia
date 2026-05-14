27815787c8ddc207f16e9ef6331ac06e619d39de
Chihyun Song <ian0371@gmail.com>
2026-05-14 09:14:40 +0900

    Remove vrank flags

--- cmd/utils/flaggroup.go
+++ cmd/utils/flaggroup.go
@@ -371,12 +371,6 @@ var FlagGroups = []FlagGroup{
 			DocRootFlag,
 		},
 	},
-	{
-		Name: "VRANK",
-		Flags: []cli.Flag{
-			VRankLogFrequencyFlag,
-		},
-	},
 }
 
 // CategorizeFlags classifies each flag into pre-defined flagGroups.
